b5e134a373da9b824bf176607bfdcaac37ad2d62
kenji-yt <kenjiyiptong@gmail.com>
2023-11-02 16:26:45 +0100

    fix: BIG change. One edgeR rule doing all.

--- workflow/rules/differential_expression.smk
+++ workflow/rules/differential_expression.smk
@@ -1,37 +1,14 @@
-rule get_counts_1:
-    output:
-        gene_count1=f"{OUTPUT_DIR}/edgeR/{{sample}}/{{sample}}_subgenome_1.input_edgeR"
-    input:
-        count_table1=f"{OUTPUT_DIR}/featureCounts/{{sample}}/{{sample}}_subgenome_1.featureCounts",
-    shell:
-        """
-        cat {input.count_table1} | awk '{if (NR>2) {print{$1 "\t" $7}}' > {output.gene_count1}
-        """
-rule get_counts_2:
-    output:
-        gene_count2=f"{OUTPUT_DIR}/edgeR/{{sample}}/{{sample}}_subgenome_2.input_edgeR"
-    input:
-        count_table2=f"{OUTPUT_DIR}/featureCounts/{{sample}}/{{sample}}_subgenome_2.featureCounts",
-    shell:
-        """
-        cat {input.count_table2} | awk '{if (NR>2) {print $1 "\\t" $7}}' > {output.gene_count2}
-        """
-
-
-
 rule edgeR:
     input:
-        gene_count1=f"{OUTPUT_DIR}/edgeR/{{sample}}/{{sample}}_subgenome_1.input_edgeR",
-        gene_count2=f"{OUTPUT_DIR}/edgeR/{{sample}}/{{sample}}_subgenome_2.input_edgeR",
+        count_dir=f"{OUTPUT_DIR}/featureCounts",
     output: 
-        sub_1=f"{OUTPUT_DIR}/edgeR/subgenome_1",
+        results=f"{OUTPUT_DIR}/edgeR/subgenome_{{one_or_two}}/{{outfile}}",
     log:
         "logs/edgeR/edgeR.log",
     params:
-        count_dir=f"{OUTPUT_DIR}/featureCounts",
         out_dir=f"{OUTPUT_DIR}/edgeR",
 
     threads: workflow.cores
     shell:
-        "Rscript workflow/scripts/edgeR.R {config[METADATA]} {params.count_dir} {config[MIN_COUNT]} {params.out_dir}"
+        "Rscript workflow/scripts/edgeR.R {config[METADATA]} {input.count_dir} {config[MIN_COUNT]} {params.out_dir}"
         
--- workflow/scripts/edgeR.R
+++ workflow/scripts/edgeR.R
@@ -26,26 +26,35 @@ dir.create(paste0(out_dir,"/subgenome_1"))
 out_1 <- paste0(out_dir,"/subgenome_1")
 dir.create(paste0(out_dir,"/subgenome_2"))
 out_2 <- paste0(out_dir,"/subgenome_2")
-# All other arguments
-Others <- comm_args[5:]
+
 
 #-------------------#
 #   Load the data   # 
 #-------------------#
 
-# create paths 
+# create edgeR formated data
+
 filenames_1 <- c() 
 filenames_2 <- c()
-groups <-c()
+
 for(i in 1:nrow(metadata)){
-    sample <- metadata$name[i]
-    filenames_1 <- c(filenames_1,paste0(sample,"/",sample,"_subgenome_",1,".input_edgeR"))
-    filenames_2 <- c(filenames_2,paste0(sample,"/",sample,"_subgenome_",2,".input_edgeR"))
+  sample <- metadata$name[i]
+  dir.create(paste0(out_dir,"/",sample))
+  
+  count_1 <- read.table(paste0(count_dir,"/",sample,"/",sample,"_subgenome_1.featureCounts"), skip = 2) 
+  name_1 <- paste0(sample,"/",sample,"_subgenome_",1,".input_edgeR")
+  write.table(count_1[,c(1,7)],paste0(out_dir,"/",name_1),quote=F,sep="\t",row.names = F,col.names = F)
+  filenames_1 <- c(filenames_1,name_1)
+  
+  count_2 <- read.table(paste0(count_dir,"/",sample,"/",sample,"_subgenome_2.featureCounts"), skip = 2) 
+  name_2 <- paste0(sample,"/",sample,"_subgenome_",2,".input_edgeR")
+  write.table(count_2[,c(1,7)],paste0(out_dir,"/",name_2),quote=F,sep="\t",row.names = F,col.names = F)     
+  filenames_2 <- c(filenames_2,name_2)
 }
 
 # read data into DGEList ojects
-sub_genome_1 <- readDGE(filenames_1, path=count_dir, columns=c(1,2), group=metadata$group, labels=metadata$name)
-sub_genome_2 <- readDGE(filenames_2, path=count_dir, columns=c(1,2), group=metadata$group, labels=metadata$name)
+sub_genome_1 <- readDGE(filenames_1, path=out_dir, columns=c(1,2), group=metadata$group, labels=metadata$name)
+sub_genome_2 <- readDGE(filenames_2, path=out_dir, columns=c(1,2), group=metadata$group, labels=metadata$name)
 
 #-----------------------------#
 #   Filter out genes by cpm   # 
@@ -74,13 +83,13 @@ sub_genome_2 <- calcNormFactors(sub_genome_2)
 #-----------------------------#
 #             MDS             # 
 #-----------------------------#
-mds_plot_sub1 <- paste0(out_1,"/MDS_1.png")
+mds_plot_sub1 <- paste0(out_1,"/MDS.png")
 png(mds_plot_sub1)
 plotMDS(sub_genome_1, col=as.numeric(sub_genome_1$samples$group))
 legend("bottomleft", as.character(unique(sub_genome_1$samples$group)), col=1:3, pch=20)
 dev.off()
 
-mds_plot_sub2 <- paste0(out_1,"/MDS_2.png")
+mds_plot_sub2 <- paste0(out_2,"/MDS.png")
 png(mds_plot_sub2)
 plotMDS(sub_genome_2, col=as.numeric(sub_genome_2$samples$group))
 legend("bottomleft", as.character(unique(sub_genome_2$samples$group)), col=1:3, pch=20)
