45cb5cdecdaddecac0248276b17a6ab987a2cc47
kenji-yt <kenjiyiptong@gmail.com>
2023-11-29 15:48:41 +0100

    RBH advances

---
+++ workflow/scripts/homeo_ratio/homeo_ratio.sh
@@ -0,0 +1,9 @@
+#############################
+#### Homeolog ratio test ####
+#############################
+
+
+# GET THE LIST OF HOMEOLOGS USING USING RBH
+###########################################
+
+bash workflow/scripts/rbh.sh 
